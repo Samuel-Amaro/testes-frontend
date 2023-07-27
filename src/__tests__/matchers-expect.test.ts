1ad51b61d158f17ce3fd3cb7e7019363ac184a44
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-27 16:45:22 -0300

    add comentario de explicação

--- src/__tests__/matchers-expect.test.ts
+++ src/__tests__/matchers-expect.test.ts
@@ -10,6 +10,13 @@ import sum from "../sum";
 //a function expect(value) é usada toda vez que voce quer testar um valor, usamos o expect junto com uma function matcher para verificar algo sobre o valor
 //o argumento para exepct deve ser o valor que o seu codigo produz, e qualquer argumento para o matcher deve ser o valor correto esperado
 
+//test(name, fn, timeout) -> metodo de teste
+//Também pode ser chamada com it(name, fn, timeout)
+//Tudo que você precisa em um arquivo de teste é o método test que executa um teste.
+//name argumento - e o nome do teste
+//fn argumento - e uma função que contém as expectativas para testar
+//timeout argumento opcional - e o timeout (em miliseegundos) que determina o tempo de espera antes da execução ser abortada 
+
 test("adds 1 + 2 to equal 3", () => {
   //aqui o toBe e um matcher que testa valores de maneira diferente
   //aqui estamos testando uma igualdade exata, isso e se a soma retorna 3
