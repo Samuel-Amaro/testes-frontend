abaed8c1e5875a79caead11e67aea207ddcf49f1
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-01 13:44:50 -0300

    configuração padrão webpack para lidar com assets estaticos e estilos em componentes

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 import type { Config } from "jest";
+import { defaults } from "jest-config";
 
 const config: Config = {
   clearMocks: true,
   testEnvironment: "jsdom" /*node*/,
   preset: "ts-jest",
+  verbose: true,
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  moduleDirectories: ["node_modules", "bower_components", "shared"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
 };
 
 export default config;
