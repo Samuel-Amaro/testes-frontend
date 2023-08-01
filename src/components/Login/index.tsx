c5d280e16aca4eee7d668b89f6c0e9db6cea9531
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-01 16:56:36 -0300

    novo componente Login

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div id="app">
+      <form action="#">
+        <label htmlFor="username-input">Username</label>
+        <input id="username-input" />
+      </form>
+    </div>
+  );
+}
