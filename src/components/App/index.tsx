d4e3651cec811ff43e76327f8f750d3bdf37257c
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-01 16:57:44 -0300

    add estrutura html e componente Login

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import Login from "../Login";
+import "./App.css";
 
 function App() {
   return (
-    <h1>Testes Front-End</h1>
-  )
+    <>
+      <h1>Testes Front-End</h1>
+      <Login />
+      <div>Olá, Mundo!</div>
+    </>
+  );
 }
 
-export default App
+export default App;
